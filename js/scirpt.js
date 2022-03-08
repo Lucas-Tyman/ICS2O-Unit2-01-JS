489d65c68312f2eae30bb58f1de170c8f92aff75
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-03-08 18:47:19 +0000

    Everything

---
+++ js/scirpt.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Lucas Tyman All rights reserved
+//
+// Created by: Lucas Tyman
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
